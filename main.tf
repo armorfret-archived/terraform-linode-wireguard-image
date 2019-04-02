1a87a5d0e0411f99e5103a40dd55405e98dbaf7a
Les Aker <me@lesaker.org>
2019-04-02 18:04:20 +0100

    fix module callout

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ resource "random_id" "vm_suffix" {
 
 module "vm" {
   source          = "armorfret/wireguard-base/linode"
-  version         = "0.0.4"
+  version         = "0.0.5"
   name            = "wg-image_${random_id.vm_suffix.hex}"
   region          = "us-east"
   type            = "g6-standard-1"
   deploy_repo     = "${var.deploy_repo}"
   source_image_id = "linode/ubuntu18.04"
   ssh_keys        = ["${var.ssh_keys}"]
-  users           = ["${var.users}"]
+  ssh_users       = ["${var.ssh_users}"]
 }
 
 resource "linode_image" "image" {
